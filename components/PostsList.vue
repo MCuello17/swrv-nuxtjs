893af143a6b783e36c0a147cc8b1d8b93e1a9684
Michael Cuello <mjcuello17@gmail.com>
2021-02-25 18:55:42 -0300

    Added result count

--- components/PostsList.vue
+++ components/PostsList.vue
@@ -1,5 +1,11 @@
 <template>
   <article>
+    <p>
+      <strong>
+        {{ posts.length }}
+      </strong>
+      results:
+    </p>
     <loading-alert v-if="!posts" class="max-w-screen-xl text-xl mx-auto my-20 flex items-center justify-center" />
     <posts-list-cards v-else :posts="posts || []" />
   </article>
